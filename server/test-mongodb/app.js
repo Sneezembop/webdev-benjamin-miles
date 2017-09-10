5a8f36ae0397d7277c61524df1a545b5051e5856
Miles Benjamin <louren.of.courone@gmail.com>
2017-09-10 11:18:19 -0400

    Connecting Mongodb to Heroku

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -9,7 +9,17 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
 
-  var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+       var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+       var password = process.env.MLAB_PASSWORD_WEBDEV;
+       connectionString = 'mongodb://' + username + ':' + password;
+       connectionString += '@ds129024.mlab.com:29024/heroku_sjjr76dr'; // use yours
+    }
+
+    //@ds129024.mlab.com:29024/heroku_sjjr76dr
+
+
 
   var mongoose = require("mongoose");
     mongoose.connect(connectionString);
