3d50b547b95489cdeca301873e1a751c861342df
Miles Benjamin <louren.of.courone@gmail.com>
2017-10-10 11:23:35 -0400

    creating services

---
+++ src/app/services/page.service.client.ts
@@ -0,0 +1,66 @@
+import {Injectable} from '@angular/core';
+import {Http, RequestOptions, Response} from '@angular/http';
+import 'rxjs/Rx';
+import {environment} from '../../environments/environment';
+import {Router} from '@angular/router';
+
+// injecting service into module
+@Injectable()
+
+export class PageService {
+
+  constructor() {
+  }
+
+  pages = [
+    { _id: '321', name: 'Post 1', websiteId: '456', description: 'Lorem' },
+    { _id: '432', name: 'Post 2', websiteId: '456', description: 'Lorem' },
+    { _id: '543', name: 'Post 3', websiteId: '456', description: 'Lorem' }
+  ];
+
+  api = {
+    'createPage': this.createPage,
+    'findPagesByWebsiteId': this.findPagesByWebsiteId,
+    'findPageById': this.findPageById,
+    'updatePage': this.updatePage,
+    'deletePage': this.deletePage
+  };
+
+
+  createPage(websiteId, page) {
+    page.developerId = websiteId;
+    this.pages.push(page);
+    return page;
+  }
+
+  findPagesByWebsiteId(websiteId) {
+    const tempPageList = [];
+    for (let x = 0; x < this.pages.length; x++) {
+      if (this.pages[x].websiteId === websiteId) {  tempPageList.push(this.pages[x]); }
+    }
+    return tempPageList;
+  }
+
+  findPageById(pageId) {
+    for (let x = 0; x < this.pages.length; x++) {
+      if (this.pages[x]._id === pageId) {  return this.pages[x]; }
+    }
+  }
+
+  updatePage(pageId, page) {
+    for (let x = 0; x < this.pages.length; x++) {
+      if (this.pages[x]._id === pageId) {
+        this.pages[x]._id = page._id;
+        this.pages[x].name = page.name;
+        this.pages[x].websiteId = page.websiteId;
+        this.pages[x].description = page.description;
+        }
+      }
+    }
+
+  deletePage(pageId) {
+    for (let x = 0; x < this.pages.length; x++) {
+      if (this.pages[x]._id === pageId) {  this.pages.splice(x, 1); }
+    }
+  }
+}
